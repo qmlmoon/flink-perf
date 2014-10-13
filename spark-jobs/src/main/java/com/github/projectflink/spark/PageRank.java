4a39f54d45a1ce73e7ecadbce29db5dd0f674c17
mingliang <qmlmoon@gmail.com>
2014-10-13 19:04:33 +0200

    Spark pagerank still slow

--- spark-jobs/src/main/java/com/github/projectflink/spark/PageRank.java
+++ spark-jobs/src/main/java/com/github/projectflink/spark/PageRank.java
@@ -1,6 +1,7 @@
 package com.github.projectflink.spark;
 
 
+import com.google.common.collect.Iterables;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -12,6 +13,7 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PageRank {
 
@@ -28,57 +30,48 @@ public class PageRank {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
 		// get input data
-		JavaRDD<Long> pagesInput = sc
+		JavaRDD<Integer> pagesInput = sc
 			.textFile(pagesInputPath)
-			.map(new Function<String, Long>() {
+			.map(new Function<String, Integer>() {
 				@Override
-				public Long call(String s) throws Exception {
-					return Long.parseLong(s);
+				public Integer call(String s) throws Exception {
+					return Integer.parseInt(s);
 				}
 			});
-		JavaPairRDD<Long, Long> linksInput = sc
+		JavaPairRDD<Integer, Integer> linksInput = sc
 			.textFile(linksInputPath)
-			.mapToPair(new PairFunction<String, Long, Long>() {
+			.mapToPair(new PairFunction<String, Integer, Integer>() {
 				@Override
-				public Tuple2<Long, Long> call(String s) throws Exception {
+				public Tuple2<Integer, Integer> call(String s) throws Exception {
 					String [] line = s.split(" ");
-					return new Tuple2<Long, Long>(Long.parseLong(line[0]), Long.parseLong(line[1]));
+					return new Tuple2<Integer, Integer>(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
 				}
 			});
 
 		// assign initial rank to pages
-		JavaPairRDD<Long, Double> pagesWithRanks = pagesInput
+		JavaPairRDD<Integer, Double> pagesWithRanks = pagesInput
 			.mapToPair(new RankAssigner(numPages));
 
 		// build adjacency list from link input
-		JavaPairRDD<Long, Long[]> adjacencyListInput = linksInput
+		JavaPairRDD<Integer, Iterable<Integer>> adjacencyListInput = linksInput
 			.groupByKey()
-			.mapToPair(new PairFunction<Tuple2<Long, Iterable<Long>>, Long, Long[]>() {
-				private final ArrayList<Long> neighbors = new ArrayList<Long>();
-				@Override
-				public Tuple2<Long, Long[]> call(Tuple2<Long, Iterable<Long>> t) throws Exception {
-					neighbors.clear();
-					for (Long n : t._2()) {
-						neighbors.add(n);
-					}
-					return new Tuple2<Long, Long[]>(t._1(), neighbors.toArray(new Long[neighbors.size()]));
-				}
-			})
 			.cache();
 
 		for (int i = 0; i < maxIterations; i++) {
 			// join pages with outgoing edges and distribute rank
-			JavaPairRDD<Long, Double> rankToDistribute = pagesWithRanks
+			JavaPairRDD<Integer, Double> rankToDistribute = pagesWithRanks
 				.join(adjacencyListInput)
-				.flatMapToPair(new PairFlatMapFunction<Tuple2<Long, Tuple2<Double, Long[]>>, Long, Double>() {
-					private final ArrayList<Tuple2<Long, Double>> rankDelta = new ArrayList<Tuple2<Long, Double>>();
+				.values()
+				.flatMapToPair(new PairFlatMapFunction<Tuple2<Double, Iterable<Integer>>, Integer, Double>() {
 					@Override
-					public Iterable<Tuple2<Long, Double>> call(Tuple2<Long, Tuple2<Double, Long[]>> t) throws Exception {
-						rankDelta.clear();
-						double r = t._2()._1() / t._2()._2().length;
+					public Iterable<Tuple2<Integer, Double>> call(Tuple2<Double, Iterable<Integer>> t) throws Exception {
+						List<Tuple2<Integer, Double>> rankDelta = new ArrayList<Tuple2<Integer, Double>>();
+						int urlCount = Iterables.size(t._2());
 
-						for (int j = 0; j < t._2()._2().length; j++) {
-							rankDelta.add(new Tuple2<Long, Double>(t._2()._2()[j], r));
+						double r = t._1() / urlCount;
+
+						for (Integer s : t._2()) {
+							rankDelta.add(new Tuple2<Integer, Double>(s, r));
 						}
 						return rankDelta;
 					}
@@ -99,32 +92,32 @@ public class PageRank {
 
 	}
 
-	public static final class RankAssigner implements PairFunction<Long, Long, Double> {
+	public static final class RankAssigner implements PairFunction<Integer, Integer, Double> {
 
 		private double rank;
 
-		public RankAssigner(Long numVertices){
+		public RankAssigner(Integer numVertices){
 			this.rank = 1.0d / numVertices;
 		}
 
 		@Override
-		public Tuple2<Long, Double> call(Long a) throws Exception {
-			return new Tuple2<Long, Double>(a, rank);
+		public Tuple2<Integer, Double> call(Integer a) throws Exception {
+			return new Tuple2<Integer, Double>(a, rank);
 		}
 	}
 
-	public static final class Dampener implements PairFunction<Tuple2<Long, Double>, Long, Double> {
+	public static final class Dampener implements PairFunction<Tuple2<Integer, Double>, Integer, Double> {
 
 		private final double dampening;
 		private final double randomJump;
 
-		public Dampener(double dampening, Long numVertices) {
+		public Dampener(double dampening, Integer numVertices) {
 			this.dampening = dampening;
 			this.randomJump = (1 - dampening) / numVertices;
 		}
 		@Override
-		public Tuple2<Long, Double> call(Tuple2<Long, Double> t) throws Exception {
-			return new Tuple2<Long, Double>(t._1(), t._2() * dampening + randomJump);
+		public Tuple2<Integer, Double> call(Tuple2<Integer, Double> t) throws Exception {
+			return new Tuple2<Integer, Double>(t._1(), t._2() * dampening + randomJump);
 		}
 	}
 
@@ -138,7 +131,7 @@ public class PageRank {
 	private static String pagesInputPath;
 	private static String linksInputPath;
 	private static String outputPath;
-	private static long numPages;
+	private static int numPages;
 	private static int maxIterations;
 
 	private static boolean parseParameters(String[] programArguments) {
